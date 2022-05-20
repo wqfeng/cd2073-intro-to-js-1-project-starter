7d90581de9da8ffe1d7c1a9f5973aff9e28a3de7
wqfeng <qunfengw@gmail.com>
2022-05-20 17:28:55 +0800

    Update script.test.js

--- starter/tests/script.test.js
+++ starter/tests/script.test.js
@@ -1,82 +1,78 @@
-const cart  = require('../src/assets/script.js')
+const cart = require("../src/assets/script.js");
 
 describe("Cart Functionality Tests", () => {
-    let product1 = cart.productsArr[1];
-    let cartArr = cart.cartArr;
+  let product1 = cart.productsArr[1];
+  let cartArr = cart.cartArr;
 
-    test("addToCart adds product to cart", () => {
-        cart.addToCart(product1.SKU); 
-        expect(product1.quantity).toEqual(1);
-        expect(cartArr).toEqual([product1]);
-    })
-    test("addToCart a second time does not append the item twice", () => {
-        cart.addToCart(product1.SKU); 
-        expect(product1.quantity).toEqual(2);
-        expect(cartArr).toEqual([product1]);
-    })
-    test("increase product quantity", () => {
-        cart.increase(product1.SKU); 
-        expect(product1.quantity).toEqual(3);
-    })
-    test("increase a second time", () => {
-        cart.increase(product1.SKU); 
-        expect(product1.quantity).toEqual(4);
-    })
-    test("decrease quantity from 4 to 1 items", () => {
-        cart.decrease(product1.SKU);
-        cart.decrease(product1.SKU);
-        cart.decrease(product1.SKU); 
-        expect(product1.quantity).toEqual(1);
-    })
-    test("decrease quantity from 1 to 0 removes item from cart", () => {
-        cart.decrease(product1.SKU); 
-        expect(product1.quantity).toEqual(0);
-        expect(cartArr).toEqual([]);
-    })
-    test("remove 1 item from cart updates quantity to 0 and removes from cart", () => {
-        cart.addToCart(product1.SKU); 
-        cart.remove(product1.SKU);
-        expect(product1.quantity).toEqual(0);
-        expect(cartArr).toEqual([]);
-    })
-
-})
-
-
+  test("addToCart adds product to cart", () => {
+    cart.addToCart(product1.SKU);
+    expect(product1.quantity).toEqual(1);
+    expect(cartArr).toEqual([product1]);
+  });
+  test("addToCart a second time does not append the item twice", () => {
+    cart.addToCart(product1.SKU);
+    expect(product1.quantity).toEqual(2);
+    expect(cartArr).toEqual([product1]);
+  });
+  test("increase product quantity", () => {
+    cart.increase(product1.SKU);
+    expect(product1.quantity).toEqual(3);
+  });
+  test("increase a second time", () => {
+    cart.increase(product1.SKU);
+    expect(product1.quantity).toEqual(4);
+  });
+  test("decrease quantity from 4 to 1 items", () => {
+    cart.decrease(product1.SKU);
+    cart.decrease(product1.SKU);
+    cart.decrease(product1.SKU);
+    expect(product1.quantity).toEqual(1);
+  });
+  test("decrease quantity from 1 to 0 removes item from cart", () => {
+    cart.decrease(product1.SKU);
+    expect(product1.quantity).toEqual(0);
+    expect(cartArr).toEqual([]);
+  });
+  test("remove 1 item from cart updates quantity to 0 and removes from cart", () => {
+    cart.addToCart(product1.SKU);
+    cart.remove(product1.SKU);
+    expect(product1.quantity).toEqual(0);
+    expect(cartArr).toEqual([]);
+  });
+});
 
 describe("Checkout Functionality Tests", () => {
-    let product1 = cart.productsArr[1];
-    let product2 = cart.productsArr[2];
-    let cartArr = cart.cartArr;
+  let product1 = cart.productsArr[1];
+  let product2 = cart.productsArr[2];
+  let cartArr = cart.cartArr;
 
-    // Functiion get's grand total of cart
-    function grandTotal(){
-        let cartSum = 0;
-        
-        for (let i = 0; i < cartArr.length; i++){
-            let itemTotal = cartArr[i].quantity * cartArr[i].price;
-            
-            cartSum += itemTotal;
-        }
-        
-        return cartSum;
+  // Functiion get's grand total of cart
+  function grandTotal() {
+    let cartSum = 0;
+
+    for (let i = 0; i < cartArr.length; i++) {
+      let itemTotal = cartArr[i].quantity * cartArr[i].price;
+
+      cartSum += itemTotal;
     }
-    
-    test("cartTotal gets grand total of cart", () =>{
-        cart.addToCart(product1.SKU);
-        cart.addToCart(product2.SKU);
-        cart.increase(product1.SKU);
-        expect(cart.cartTotal()).toEqual(grandTotal());
-    })
 
-    test("pay more than the total works", () =>{
-        expect(cart.pay(100000000000000)).toBeGreaterThan(grandTotal());
-    })
+    return cartSum;
+  }
 
-    test("pay less than the total works", () =>{
-        cart.addToCart(product1.SKU);
-        cart.addToCart(product2.SKU);
-        expect(cart.pay(1)).toBeLessThan(grandTotal());
-    })
-})
-    
+  test("cartTotal gets grand total of cart", () => {
+    cart.addToCart(product1.SKU);
+    cart.addToCart(product2.SKU);
+    cart.increase(product1.SKU);
+    expect(cart.cartTotal()).toEqual(grandTotal());
+  });
+
+  test("pay less than the total works", () => {
+    cart.addToCart(product1.SKU);
+    cart.addToCart(product2.SKU);
+    expect(cart.pay(1)).toBeLessThan(grandTotal());
+  });
+
+  test("pay more than the total works", () => {
+    expect(cart.pay(100000000000000)).toBeGreaterThan(grandTotal());
+  });
+});
